d69a3e53e1b782f85b4160fa0b2eace0aa438b9b
Rohan James <rohanjames9282@gmail.com>
2022-08-15 21:35:00 +0530

    Add is_winner function

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,6 +4,7 @@ require 'pry-byebug'
 # CLASSES
 # ------------------------#
 class Game
+  include  CheckFunctions
   @@rounds = 0
   @@game_grids = {1 => 1,
     2 => 2,
@@ -14,29 +15,35 @@ class Game
     7 => 7,
     8 => 8,
     9 => 9,}
+
     def round
       puts "#{name}, please enter a number to place your symbol(#{symbol})",
-      "    #{@@game_grids[1]} | #{@@game_grids[2]} | #{@@game_grids[3]}
+      "      #{@@game_grids[1]} | #{@@game_grids[2]} | #{@@game_grids[3]}
       --+---+--
       #{@@game_grids[4]} | #{@@game_grids[5]} | #{@@game_grids[6]}
       --+---+--
       #{@@game_grids[7]} | #{@@game_grids[8]} | #{@@game_grids[9]}
       "
       @@game_grids[gets.to_i] = symbol
-    @@rounds += 1
+      @@rounds += 1
+      case true
+      when is_winner?
+        puts "Congratulations #{name}! You have Won the GAME!!!"
+      when rounds == 9
+        puts "This game was a draw. Wanna try again?"
+      end
   end
 end
 
 class Player < Game
 
-  private
-
   attr_accessor :name, :symbol
+
   def initialize()
     puts "Please enter a name"
     name = gets.chomp.capitalize
     puts "#{name}, please chose your symbol"
-    symbol = gets.chomp.to_s
+    symbol = gets.chomp
       @name = name
       @symbol = symbol
     end
@@ -45,6 +52,29 @@ end
 # ------------------------#
 # MODULES
 # ------------------------#
+module CheckFunctions
+  def is_winner?
+    #This function checks if 3 user symbol gets aligned together
+    case true
+      #For horizontal alignment
+    when @@game_grids[1] == @@game_grids[2] && @@game_grids[2] == @@game_grids[3] #1, 2, and 3
+    when @@game_grids[4] == @@game_grids[5] && @@game_grids[5] == @@game_grids[6] #4, 5, and 6
+    when @@game_grids[7] == @@game_grids[8] && @@game_grids[8] == @@game_grids[9] #7, 8, and 9
+     #For vertical alignment
+    when @@game_grids[1] == @@game_grids[4] && @@game_grids[4] == @@game_grids[7] #1,4, and 7
+    when @@game_grids[2] == @@game_grids[5] && @@game_grids[5] == @@game_grids[6] #2,5, and 8
+    when @@game_grids[3] == @@game_grids[6] && @@game_grids[6] == @@game_grids[9] #3,6, and 9
+      #For cross alignment
+    when @@game_grids[1] == @@game_grids[5] && @@game_grids[5] == @@game_grids[9] #1,5, and 9
+    when @@game_grids[3] == @@game_grids[5] && @@game_grids[5] == @@game_grids[7] #3,5, and 7
+      return true
+    else
+      return false
+    end
+  end
+
+  
+end
 
 
 # ------------------------#
