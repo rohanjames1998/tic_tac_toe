345c9ff8eceb4f87e020ba7cf44a08ca0e52d34d
Rohan James <rohanjames9282@gmail.com>
2022-08-15 19:39:35 +0530

    Fix symbol not showing on the game grid

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,16 +4,26 @@ require 'pry-byebug'
 # CLASSES
 # ------------------------#
 class Game
-	attr_accessor :sym
-  def round
-    @sym = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    puts "#{name}, please enter a number to place your symbol",
-         "    #{sym[0]} | #{sym[1]} | #{sym[2]}
-    --+---+--
-    #{sym[3]} | #{sym[4]} | #{sym[5]}
-    --+---+--
-    #{sym[6]} | #{sym[7]} | #{sym[8]}
-"
+  @@rounds = 0
+  @@game_grids = {1 => 1,
+    2 => 2,
+    3 => 3,
+    4 => 4,
+    5 => 5,
+    6 => 6,
+    7 => 7,
+    8 => 8,
+    9 => 9,}
+    def round
+      puts "#{name}, please enter a number to place your symbol(#{symbol})",
+      "    #{@@game_grids[1]} | #{@@game_grids[2]} | #{@@game_grids[3]}
+      --+---+--
+      #{@@game_grids[4]} | #{@@game_grids[5]} | #{@@game_grids[6]}
+      --+---+--
+      #{@@game_grids[7]} | #{@@game_grids[8]} | #{@@game_grids[9]}
+      "
+      @@game_grids[gets.to_i] = symbol
+    @@rounds += 1
   end
 end
 
@@ -25,8 +35,8 @@ class Player < Game
   def initialize()
     puts "Please enter a name"
     name = gets.chomp.capitalize
-    puts "Please chose your symbol"
-    symbol = gets.chomp
+    puts "#{name}, please chose your symbol"
+    symbol = gets.chomp.to_s
       @name = name
       @symbol = symbol
     end
@@ -41,25 +51,17 @@ end
 # GAME FUNCTION
 # ------------------------#
 
-new_game = true
+puts "Hello there! Welcome to Tic Tac Toe!",
+     "Player 1"
+     p1 = Player.new
+puts  "Player 2"
+     p2 = Player.new
+
 loop do
   quit_game = false
-  if new_game == true
-  puts "Hello there! Welcome to Tic Tac Toe!"
-       p1 = Player.new
-       p2 = Player.new
-       new_game = false
-  end
-			#  p1.round
 
-
-
-
-
-
-
-
-
+       p1.round
+       p2.round
 
   break if quit_game == true
 end
