c1ecef145e6810145bfb485a6463c15fc2a743f0
Rohan James <rohanjames9282@gmail.com>
2022-08-15 19:01:16 +0530

    Add gets funtionality to player class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,14 +3,32 @@ require 'pry-byebug'
 # ------------------------#
 # CLASSES
 # ------------------------#
-class Player
-    attr_accessor :name, :symbol
-    def initialize(name)
-        @name = name
-    end
+class Game
+	attr_accessor :sym
+  def round
+    @sym = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    puts "#{name}, please enter a number to place your symbol",
+         "    #{sym[0]} | #{sym[1]} | #{sym[2]}
+    --+---+--
+    #{sym[3]} | #{sym[4]} | #{sym[5]}
+    --+---+--
+    #{sym[6]} | #{sym[7]} | #{sym[8]}
+"
+  end
+end
 
-    def symbol=(character)
-        @symbol = character
+class Player < Game
+
+  private
+
+  attr_accessor :name, :symbol
+  def initialize()
+    puts "Please enter a name"
+    name = gets.chomp.capitalize
+    puts "Please chose your symbol"
+    symbol = gets.chomp
+      @name = name
+      @symbol = symbol
     end
 end
 
@@ -23,10 +41,17 @@ end
 # GAME FUNCTION
 # ------------------------#
 
+new_game = true
 loop do
   quit_game = false
-  puts "Hello there! Welcome to Tic Tac Toe!", \
-       "Please enter your name"
+  if new_game == true
+  puts "Hello there! Welcome to Tic Tac Toe!"
+       p1 = Player.new
+       p2 = Player.new
+       new_game = false
+  end
+			#  p1.round
+
 
 
 
@@ -38,3 +63,4 @@ loop do
 
   break if quit_game == true
 end
+
