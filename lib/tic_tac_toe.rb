2293d040dd723110a5be6dc9eac36e352248a729
Rohan James <rohanjames9282@gmail.com>
2022-10-14 18:20:51 +0530

    Rearrange methods according to their calls in the game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 require_relative './player'
 
 class TicTacToe
-  attr_accessor :rounds, :game_grids, :end_game, :p1, :p2
+  attr_accessor :rounds, :game_grids, :p1, :p2
 
   def initialize
     @rounds = 0
@@ -15,7 +15,6 @@ class TicTacToe
                     7 => 7,
                     8 => 8,
                     9 => 9, }
-    @end_game = false
   end
 
   def start_game
@@ -37,37 +36,6 @@ class TicTacToe
     play_game(p1, p2)
   end
 
-  def round(player)
-    display_grid(player)
-    # Checks for winning and drawing conditions
-  end
-
-  def display_grid(player)
-    # Grid for each round
-    puts "\n\n#{player.name}, please enter a number to place your symbol(#{player.symbol})",
-         "      #{game_grids[1]} | #{game_grids[2]} | #{game_grids[3]}
-  --+---+--
-  #{game_grids[4]} | #{game_grids[5]} | #{game_grids[6]}
-  --+---+--
-  #{game_grids[7]} | #{game_grids[8]} | #{game_grids[9]}
-  "
-    # Function that asks user to choose a grid to place symbol and checks if it is a valid
-    # location and it doesn't already contain a symbol
-    place_symbol_on_grid(game_grids)
-    rounds += 1
-  end
-
-  def end_game?
-    case
-    when is_winner?(game_grids)
-      puts "\nCongratulations #{player.name}! You have Won the GAME!!!"
-      return true
-    when rounds == 9
-      puts "\nIt's a draw :("
-      return true
-    end
-  end
-
   def play_game(player_1, player_2)
     # Game Loop
     # We need to check after each round to see if someone has won or if its a draw.
@@ -81,23 +49,81 @@ class TicTacToe
     restart_game
   end
 
+  def round(player)
+    display_grid(player)
+    place_symbol_on_grid(game_grids, player)
+  end
+
+    # Function that asks user to choose a grid to place symbol and checks if it is a valid
+    # location and it doesn't already contain a symbol
+    def place_symbol_on_grid(grid, player)
+      loop do
+        place = symbol_place(player)
+        # This is to make sure player doesn't put symbol where one is already present.
+        if grid[place].is_a?(Integer)
+          return grid[place] = symbol
+        else
+          puts "Sorry that place already contains a symbol. Please select a new place."
+          next
+        end
+      end
+    end
+
+    # This function gets and verifies that player input is an integer.
+    def symbol_place(player)
+      loop do
+        place = gets
+        if /[0-9]/.match(place)
+          return place.to_i
+        else
+          puts "#{player.name}, please enter a number between 1 and 9 to place your symbol"
+          next
+        end
+      end
+    end
+
+  def display_grid(player)
+    # Grid for each round
+    puts "\n\n#{player.name}, please enter a number to place your symbol(#{player.symbol})",
+         "      #{game_grids[1]} | #{game_grids[2]} | #{game_grids[3]}
+  --+---+--
+  #{game_grids[4]} | #{game_grids[5]} | #{game_grids[6]}
+  --+---+--
+  #{game_grids[7]} | #{game_grids[8]} | #{game_grids[9]}
+  "
+  rounds += 1
+end
+
+
+def end_game?
+  case
+  when is_winner?(game_grids)
+      puts "\nCongratulations #{player.name}! You have Won the GAME!!!"
+      return true
+    when rounds == 9
+      puts "\nIt's a draw :("
+      return true
+    end
+    return false
+  end
+
   def restart_game
     # Option to restart the game
+    puts "Wanna play another game? [Y/N]:"
     loop do
-      puts "Wanna play another game? [Y/N]:"
       restart_game = gets.chomp.upcase
       case restart_game
       when "Y"
         # Resetting game grids and starting a new game
         game_grids = { 1 => 1,
-                       2 => 2,
-                       3 => 3,
-                       4 => 4,
-                       5 => 5,
-                       6 => 6,
-                       7 => 7,
-                       8 => 8,
-                       9 => 9, }
+          2 => 2,
+          3 => 3,
+          4 => 4,
+          5 => 5,
+          6 => 6,
+          7 => 7,
+          8 => 8,
+          9 => 9, }
         end_game = false
         rounds = 0
         play_game(p1, p2)
@@ -105,7 +131,6 @@ class TicTacToe
         return
       else
         puts "Please enter a valid input [Y/N]:"
-        restart_game = gets.chomp.upcase
         next
       end
     end
@@ -121,7 +146,7 @@ class TicTacToe
       return true
     when grid[7] == grid[8] && grid[8] == grid[9] # 7, 8, and 9
       return true
-    # For vertical alignment
+      # For vertical alignment
     when grid[1] == grid[4] && grid[4] == grid[7] # 1,4, and 7
       return true
     when grid[2] == grid[5] && grid[5] == grid[8] # 2,5, and 8
@@ -135,30 +160,6 @@ class TicTacToe
       return true
     else
       return false
-    end
-  end
-
-  def symbol_place
-    loop do
-      place = gets
-      if /[0-9]/.match(place)
-        return place.to_i
-      else
-        puts "#{name}, please enter a number between 1 and 9 to place your symbol"
-        next
-      end
-    end
-  end
-
-  def place_symbol_on_grid(grid)
-    loop do
-      place = symbol_place
-      if grid[place].is_a?(Integer)
-        return grid[place] = symbol
-      else
-        puts "Sorry that place already contains a symbol. Please select a new place."
-        next
-      end
     end
   end
 end
