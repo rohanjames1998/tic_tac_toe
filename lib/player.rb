dbbc68d1a7131e0659cf20590c9def6859ad598a
Rohan James <rohanjames9282@gmail.com>
2022-10-14 17:48:00 +0530

    Moved #get_symbol and #get_name to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,39 @@ class Player
     puts "#{name}, please chose your symbol"
     @symbol = get_symbol
   end
+
+  def get_symbol
+    loop do
+      player_symbol = gets.chomp
+      case
+      when player_symbol.length > 1
+        puts "You can only have one character as your symbol."
+        next
+      when /[0-9]/.match(player_symbol)
+        puts "Your symbol can't be an Integer"
+        next
+      when player_symbol.length == 0 || /\s+/.match(player_symbol)
+        puts "Your symbol cannot be empty"
+        next
+      else
+        return player_symbol
+      end
+    end
+  end
+
+  def get_name
+    loop do
+      player_name = gets.chomp.capitalize
+      if player_name.length > 10
+        puts "Please keep your name under 10 characters"
+        next
+      elsif player_name.length == 0 || /\s+/.match(player_name)
+        puts "Your name cannot be blank",
+             "Please enter a valid name"
+        next
+      else
+        return player_name
+      end
+    end
+  end
 end
