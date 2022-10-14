07361e059e09b90f66768b62ea2f0c99254c780b
Rohan James <rohanjames9282@gmail.com>
2022-10-14 17:58:50 +0530

    Seperated #play_game and #restart_game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ require_relative './player'
 
 class TicTacToe
 
-  attr_accessor: :rounds, :game_grids, :end_game
+  attr_accessor :rounds, :game_grids, :end_game, :p1, :p2
 
   def initialize
   @rounds = 0
@@ -19,74 +19,77 @@ class TicTacToe
   @end_game = false
   end
 
-  def self.start_game
+  def start_game
     puts "Hello there! Welcome to Tic Tac Toe!",
          "Player 1"
-    p1 = Player.new
+    @p1 = Player.new
     puts "\nPlayer 2"
-    p2 = Player.new
+    @p2 = Player.new
     # This loop checks for symbol duplications
     loop do
       if p2.symbol == p1.symbol
         puts "You cannot have the same symbol as your partner!!"
         puts "Please choose a different symbol"
-        p2.symbol = get_symbol
+        p2.symbol = p2.get_symbol
         next
-      else
-        break
       end
+      break
     end
-    Game.play_game(p1, p2)
+    play_game(p1, p2)
   end
 
   def round
     # Grid for each round
     puts "\n\n#{name}, please enter a number to place your symbol(#{symbol})",
-         "      #{@game_grids[1]} | #{@game_grids[2]} | #{@game_grids[3]}
+         "      #{game_grids[1]} | #{game_grids[2]} | #{game_grids[3]}
       --+---+--
-      #{@game_grids[4]} | #{@game_grids[5]} | #{@game_grids[6]}
+      #{game_grids[4]} | #{game_grids[5]} | #{game_grids[6]}
       --+---+--
-      #{@game_grids[7]} | #{@game_grids[8]} | #{@game_grids[9]}
+      #{game_grids[7]} | #{game_grids[8]} | #{game_grids[9]}
       "
     # Function that asks user to choose a grid to place symbol and checks if it is a valid
     # location and it doesnt already contain a symbol
-    place_symbol_on_grid(@game_grids)
-    @rounds += 1
+    place_symbol_on_grid(game_grids)
+    rounds += 1
     # Checks for winning and drawing conditions
     case true
-    when is_winner?(@game_grids)
+    when is_winner?(game_grids)
       puts "\nCongratulations #{name}! You have Won the GAME!!!"
-      @end_game = true
+      end_game = true
       return
-    when @rounds == 9
+    when rounds == 9
       puts "\nIt's a draw :("
-      @end_game = true
+      end_game = true
       return
     end
   end
 
-  def self.end_game?
-    @end_game
+  def end_game?
+    end_game
   end
 
-  def self.play_game(player_1, player_2)
+  def play_game(player_1, player_2)
     # Game Loop
     # We need to check after each round to see if someone has won or if its a draw.
     loop do
       player_1.round
-      break if Game.end_game? == true
+      break if end_game? == true
 
       player_2.round
-      break if Game.end_game? == true
+      break if end_game? == true
     end
+    restart_game
+  end
+
+  def restart_game
     # Option to restart the game
-    puts "Wanna play another game? [Y/N]:"
-    restart_game = gets.chomp.upcase
     loop do
+      puts "Wanna play another game? [Y/N]:"
+      restart_game = gets.chomp.upcase
       case restart_game
       when "Y"
         # Resetting game grids and starting a new game
-        @game_grids = { 1 => 1,
+        game_grids = { 1 => 1,
                          2 => 2,
                          3 => 3,
                          4 => 4,
@@ -95,9 +98,9 @@ class TicTacToe
                          7 => 7,
                          8 => 8,
                          9 => 9, }
-        @end_game = false
-        @rounds = 0
-        Game.start_game
+        end_game = false
+        rounds = 0
+        play_game(p1, p2)
       when "N"
         return
       else
@@ -109,7 +112,7 @@ class TicTacToe
   end
 
   def is_winner?(grid)
-    # This function checks if 3 user symbol gets aligned together
+    # This function checks if 3 user symbols gets aligned together
     case true
       # For horizontal alignment
     when grid[1] == grid[2] && grid[2] == grid[3] # 1, 2, and 3
@@ -158,47 +161,5 @@ class TicTacToe
       end
     end
   end
-
-  def get_symbol
-    loop do
-      player_symbol = gets.chomp
-      case
-      when player_symbol.length > 1
-        puts "You can only have one character as your symbol."
-        next
-      when /[0-9]/.match(player_symbol)
-        puts "Your symbol can't be an Integer"
-        next
-      when player_symbol.length == 0 || /\s+/.match(player_symbol)
-        puts "Your symbol cannot be empty"
-        next
-      else
-        return player_symbol
-      end
-    end
-  end
-
-  def get_name
-    loop do
-      player_name = gets.chomp.capitalize
-      if player_name.length > 10
-        puts "Please keep your name under 10 characters"
-        next
-      elsif player_name.length == 0 || /\s+/.match(player_name)
-        puts "Your name cannot be blank",
-             "Please enter a valid name"
-        next
-      else
-        return player_name
-      end
-    end
-  end
 end
 
-
-
-# ------------------------#
-# Game logic
-# ------------------------#
-
-Game.start_game
