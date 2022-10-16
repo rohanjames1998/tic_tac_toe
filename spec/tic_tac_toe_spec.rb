e208b2595e59430aef29dd16db65832c628ac105
Rohan James <rohanjames9282@gmail.com>
2022-10-16 14:29:25 +0530

    Add tests for #duplicate_symbol_check

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -2,24 +2,24 @@ require_relative '../lib/tic_tac_toe'
 require_relative '../lib/player'
 
 describe TicTacToe do
-  # To remove all output statements that might clutter
-  # test results.
   before do
+    # To remove all output statements that might clutter
+    # test results.
     allow(STDOUT).to receive(:puts)
+    allow(p1).to receive(:get_name)
+    allow(p1).to receive(:get_symbol)
+    allow(p2).to receive(:get_name)
+    allow(p2).to receive(:get_symbol)
   end
+  let(:p1) { instance_double(Player) }
+  let(:p2) { instance_double(Player) }
 
   describe '#start_game' do
     before do
-      allow(p1).to receive(:get_name)
-      allow(p1).to receive(:get_symbol)
-      allow(p2).to receive(:get_name)
-      allow(p2).to receive(:get_symbol)
       allow(game_start).to receive(:play_game).with(p1, p2)
       allow(game_start).to receive(:duplicate_symbol_check).with(p1, p2)
     end
     subject(:game_start) { described_class.new }
-    let(:p1) { instance_double(Player) }
-    let(:p2) { instance_double(Player) }
 
     context 'When called' do
 
@@ -39,7 +39,23 @@ describe TicTacToe do
 
   describe '#duplicate_symbol_check' do
     subject(:duplicate_sym_game) { described_class.new }
-    
+
+    context 'When both players have same symbol' do
+      it 'Loops until both have different symbols' do
+        symbol_1 = 'x'
+        symbol_2 = 'y'
+        allow(p1).to receive(:symbol).and_return(symbol_1, symbol_1,symbol_1, symbol_2)
+        allow(p2).to receive(:symbol).and_return(symbol_1)
+        expect(p2).to receive(:get_symbol).exactly(3).times
+        duplicate_sym_game.duplicate_symbol_check(p1, p2)
+      end
+    end
+  end
+
+  describe '#play_game' do
+    subject(:playing_game) { described_class.new }
+  end
+
 end
 
 
